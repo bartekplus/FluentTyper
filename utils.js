34d4520d869549b3c5ddd9c0a802acb6a3beb087
Bartosz Tomczyk <bartosz.tomczyk86@gmail.com>
2020-05-16 22:16:25 +0200

    Initial impl of popup.

---
+++ utils.js
@@ -0,0 +1,75 @@
+function getDomain(url) {
+
+    url = url.replace(/(https?:\/\/)?(www.)?/i, '');
+
+    if (url.indexOf('/') !== -1) {
+        return url.split('/')[0];
+    }
+
+    return url;
+
+}
+
+function isDomainOnList(settings, domainURL) {
+    ret = false;
+    domainURL = getDomain(domainURL);
+
+    var domainList = [];
+    domainListAsStr = settings.get("domainList");
+    if (domainListAsStr) {
+        domainList = domainListAsStr.split("|@|");
+    }
+
+    for (var i = 0; i < domainList.length; i++) {
+        if (domainURL.match(domainList[i])) {
+            ret = true;
+            break;
+        }
+    }
+    return ret;
+}
+
+
+function isDomainOnList(settings, domainURL) {
+    ret = false;
+
+    var domainList = [];
+    domainListAsStr = settings.get("domainList");
+    if (domainListAsStr) {
+        domainList = domainListAsStr.split("|@|");
+    }
+
+    for (var i = 0; i < domainList.length; i++) {
+        if (domainURL.match(domainList[i])) {
+            ret = true;
+            break;
+        }
+    }
+    return ret;
+}
+
+
+function addDomainToList(settings, domainURL) {
+    domainListAsStr = settings.get("domainList") || [];
+    domainListAsStr.push(domainURL);
+
+    settings.set("domainList", domainListAsStr.join("|@|"));
+}
+
+function removeDomainFromList(settings, domainURL) {
+
+    domainURL = getDomain(domainURL);
+    var domainList = [];
+    domainListAsStr = settings.get("domainList");
+    if (domainListAsStr) {
+        domainList = domainListAsStr.split("|@|");
+    }
+
+    for (var i = 0; i < domainList.length; i++) {
+        if (domainURL.match(domainList[i])) {
+            domainList.splice(i, 1);
+            settings.set("domainList", domainList.join("|@|"));
+            break;
+        }
+    }
+}
