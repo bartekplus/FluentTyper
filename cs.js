c914d0c06d103d503adc4ada3005ce97b5e5e29e
Bartosz Tomczyk <bartosz.tomczyk86@gmail.com>
2020-05-17 14:03:42 +0200

    Remove horsey.

--- cs.js
+++ cs.js
@@ -1,7 +1,5 @@
 (function() {
 
-    const useHorsey = false;
-    horseyArr = [];
     tributeArr = [];
     observer = null;
 
@@ -50,196 +48,119 @@
                     continue;
                 }
 
-                if (isHelperAttached(useHorsey ? horseyArr : tributeArr, elem)) {
+                if (isHelperAttached(tributeArr, elem)) {
                     continue;
                 }
-                helperArrId = useHorsey ? horseyArr.length : tributeArr.length;
+                helperArrId = tributeArr.length;
 
-                if (useHorsey) {
+                tributeArr.push({
+                    tribute: null,
+                    elem: elem,
+                    done: null,
+                    timeout: null
+                });
 
-                    helperArrId = horseyArr.length;
-                    horseyArr.push({
-                        horsey: null,
-                        done: null,
-                        elem: elem
-                    });
-                    horseyObj = horsey(elem, {
+                elem.addEventListener("tribute-replaced", function(e) {
+                    setTimeout((function() {
+                        // Triger predition after replacing text, without user interacction
+                        e.target.dispatchEvent(new KeyboardEvent('keydown', { 'key': ' ' }));
+                        e.target.dispatchEvent(new KeyboardEvent('keyup', { 'key': ' ' }));
+                    }), 10);
+                });
 
-                        source: (function(data, done) {
-                            var localId = helperArrId;
-                            return function(data, done) {
-                                horseyArr[localId].done = done;
-                                var message = {
-                                    command: 'predictReq',
-                                    context: {
-                                        text: data.input,
-                                        horseyId: localId
-                                    }
-                                };
-                                chrome.runtime.sendMessage(message, function(response) {
-                                    console.log("GOT RESPONSE : " + response);
-                                });
-                            }
-                        })(),
+                var tribute = new Tribute({
+                    // symbol or string that starts the lookup
+                    trigger: "",
 
-                        readInput: function(el) {
-                            inputStr = ""
+                    // element to target for @mentions
+                    iframe: null,
 
-                            if (el.selectionStart == el.selectionEnd) {
-                                if (el.tagName === "DIV") {
-                                    attrib = "textContent";
-                                } else {
-                                    attrib = "value"
+                    // class added in the flyout menu for active item
+                    selectClass: 'highlight',
+
+                    // class added to the menu container
+                    containerClass: 'tribute-container',
+
+                    // class added to each list item
+                    itemClass: '',
+
+                    // function called on select that returns the content to insert
+                    selectTemplate: function(item) {
+                        return item.original.value;
+                    },
+
+                    // template for displaying item in menu
+                    menuItemTemplate: function(item) {
+                        return item.string;
+                    },
+
+                    // template for when no match is found (optional),
+                    // If no template is provided, menu is hidden.
+                    noMatchTemplate: "",
+
+                    // specify an alternative parent container for the menu
+                    // container must be a positioned element for the menu to appear correctly ie. `position: relative;`
+                    // default container is the body
+                    menuContainer: document.body,
+
+                    // column to search against in the object (accepts function or string)
+                    lookup: 'key',
+
+                    // column that contains the content to insert by default
+                    fillAttr: 'value',
+
+                    // REQUIRED: array of objects to match
+                    values: (function(data, done) {
+                        var localId = helperArrId;
+                        return function(data, done) {
+                            tributeArr[localId].done = done;
+                            var message = {
+                                command: 'predictReq',
+                                context: {
+                                    text: data,
+                                    tributeId: localId
                                 }
+                            };
+                            chrome.runtime.sendMessage(message, function(response) {
+                                tributeArr[localId].timeout = setTimeout((function() { return done([]); }), 1000);
+                            });
+                        }
+                    })(),
 
-                                inputStr = el[attrib].slice(0, el.selectionStart);
-                            }
+                    // specify whether a space is required before the trigger string
+                    requireLeadingSpace: false,
 
-                            return inputStr;
-                            return (textInput ? el.value : el.innerHTML).trim();
-                        },
+                    // specify whether a space is allowed in the middle of mentions
+                    allowSpaces: false,
 
-                        filter(query, suggestion) {
-                            return suggestion;
-                        },
-                        set: (function(value) {
-                            var _elem = elem;
-                            return function(value) {
-                                attrib = "";
-                                if (_elem.tagName === "DIV") {
-                                    attrib = "textContent";
-                                } else {
-                                    attrib = "value"
-                                }
+                    // optionally specify a custom suffix for the replace text
+                    // (defaults to empty space if undefined)
+                    replaceTextSuffix: ' ',
 
-                                inputStrToSelection = _elem[attrib].slice(0, _elem.selectionStart);
-                                inputStrFromSelection = _elem[attrib].slice(_elem.selectionStart);
+                    // specify whether the menu should be positioned.  Set to false and use in conjuction with menuContainer to create an inline menu
+                    // (defaults to true)
+                    positionMenu: true,
 
+                    // when the spacebar is hit, select the current match
+                    spaceSelectsMatch: false,
 
-                                new_value = inputStrToSelection.split(' ');
-                                new_value.pop(); // Remove last elem
-                                new_value.push(value + " "); // Add suggestion and space after it.
-                                new_value = new_value.join(' ');
-                                _elem[attrib] = new_value + inputStrFromSelection; // Concat it with rest of the input
-                                // Update current position
-                                _elem.selectionStart = new_value.length;
-                                _elem.selectionEnd = new_value.length;
-                            }
-                        })(),
-                        setAppends: true
-                    })
-                    horseyArr[helperArrId].horsey = horseyObj;
-                } else {
-                    tributeArr.push({
-                        tribute: null,
-                        elem: elem,
-                        done: null,
-                        timeout: null
-                    });
+                    // turn tribute into an autocomplete
+                    autocompleteMode: true,
 
+                    // Customize the elements used to wrap matched strings within the results list
+                    // defaults to <span></span> if undefined
+                    searchOpts: {
+                        pre: '<span>',
+                        post: '</span>',
+                        skip: false // true will skip local search, useful if doing server-side search
+                    },
 
-                    elem.addEventListener("tribute-replaced", function(e) {
-                        setTimeout((function() {
-                            // Triger predition after replacing text, without user interacction
-                            e.target.dispatchEvent(new KeyboardEvent('keydown', { 'key': ' ' }));
-                            e.target.dispatchEvent(new KeyboardEvent('keyup', { 'key': ' ' }));
-                        }), 10);
-                    });
+                    // specify the minimum number of characters that must be typed before menu appears
+                    menuShowMinLength: 0
+                });
+                tributeArr[helperArrId].tribute = tribute;
+                tribute.attach(elem);
 
-
-                    var tribute = new Tribute({
-                        // symbol or string that starts the lookup
-                        trigger: "",
-
-                        // element to target for @mentions
-                        iframe: null,
-
-                        // class added in the flyout menu for active item
-                        selectClass: 'highlight',
-
-                        // class added to the menu container
-                        containerClass: 'tribute-container',
-
-                        // class added to each list item
-                        itemClass: '',
-
-                        // function called on select that returns the content to insert
-                        selectTemplate: function(item) {
-                            return item.original.value;
-                        },
-
-                        // template for displaying item in menu
-                        menuItemTemplate: function(item) {
-                            return item.string;
-                        },
-
-                        // template for when no match is found (optional),
-                        // If no template is provided, menu is hidden.
-                        noMatchTemplate: "",
-
-                        // specify an alternative parent container for the menu
-                        // container must be a positioned element for the menu to appear correctly ie. `position: relative;`
-                        // default container is the body
-                        menuContainer: document.body,
-
-                        // column to search against in the object (accepts function or string)
-                        lookup: 'key',
-
-                        // column that contains the content to insert by default
-                        fillAttr: 'value',
-
-                        // REQUIRED: array of objects to match
-                        values: (function(data, done) {
-                            var localId = helperArrId;
-                            return function(data, done) {
-                                tributeArr[localId].done = done;
-                                var message = {
-                                    command: 'predictReq',
-                                    context: {
-                                        text: data,
-                                        tributeId: localId
-                                    }
-                                };
-                                chrome.runtime.sendMessage(message, function(response) {
-                                    tributeArr[localId].timeout = setTimeout((function() { return done([]); }), 1000);
-                                });
-                            }
-                        })(),
-
-                        // specify whether a space is required before the trigger string
-                        requireLeadingSpace: false,
-
-                        // specify whether a space is allowed in the middle of mentions
-                        allowSpaces: false,
-
-                        // optionally specify a custom suffix for the replace text
-                        // (defaults to empty space if undefined)
-                        replaceTextSuffix: ' ',
-
-                        // specify whether the menu should be positioned.  Set to false and use in conjuction with menuContainer to create an inline menu
-                        // (defaults to true)
-                        positionMenu: true,
-
-                        // when the spacebar is hit, select the current match
-                        spaceSelectsMatch: false,
-
-                        // turn tribute into an autocomplete
-                        autocompleteMode: true,
-
-                        // Customize the elements used to wrap matched strings within the results list
-                        // defaults to <span></span> if undefined
-                        searchOpts: {
-                            pre: '<span>',
-                            post: '</span>',
-                            skip: false // true will skip local search, useful if doing server-side search
-                        },
-
-                        // specify the minimum number of characters that must be typed before menu appears
-                        menuShowMinLength: 0
-                    });
-                    tributeArr[helperArrId].tribute = tribute;
-                    tribute.attach(elem);
-                }
             }
 
         }
@@ -267,24 +188,18 @@
     chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
         switch (message.command) {
             case "predictResp":
-                if (useHorsey) {
-                    horseyArr[message.context.horseyId].done(null, [{
-                        list: message.context.predictions
-                    }]);
-                } else {
-                    x = [];
-                    for (var i = 0; i < message.context.predictions.length; i++) {
-                        x.push({
-                            key: message.context.predictions[i],
-                            value: message.context.predictions[i]
-                        })
-                    }
-                    if (tributeArr[message.context.tributeId].timeout) {
-                        clearTimeout(tributeArr[message.context.tributeId].timeout);
-                        tributeArr[message.context.tributeId].timeout = null;
-                    }
-                    tributeArr[message.context.tributeId].done(x);
+                keyValPairs = [];
+                for (var i = 0; i < message.context.predictions.length; i++) {
+                    keyValPairs.push({
+                        key: message.context.predictions[i],
+                        value: message.context.predictions[i]
+                    })
                 }
+                if (tributeArr[message.context.tributeId].timeout) {
+                    clearTimeout(tributeArr[message.context.tributeId].timeout);
+                    tributeArr[message.context.tributeId].timeout = null;
+                }
+                tributeArr[message.context.tributeId].done(keyValPairs);
                 break;
             default:
                 console.log("Unknown message");
