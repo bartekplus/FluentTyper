c904597ecdaabaee34ab158f9a76159961568835
Bartosz Tomczyk <bartosz.tomczyk86@gmail.com>
2020-05-18 11:54:02 +0200

    Update tribute.

--- third_party/tribute/tribute.js
+++ third_party/tribute/tribute.js
@@ -186,7 +186,8 @@
     }, {
       key: "input",
       value: function input(instance, event) {
-        instance.inputEvent = true;
+        instance.inputEvent = event instanceof CustomEvent ? false : true;
+        instance.commandEvent = !instance.inputEvent;
         instance.keyup.call(this, instance, event);
       }
     }, {
@@ -466,9 +467,6 @@
           key: 8,
           value: "DELETE"
         }, {
-          key: 13,
-          value: "ENTER"
-        }, {
           key: 27,
           value: "ESCAPE"
         }, {
@@ -736,28 +734,11 @@
         var range, sel;
         sel = this.getWindowSelection();
         range = this.getDocument().createRange();
-        range.setStart(sel.anchorNode, startPos);
-        range.setEnd(sel.anchorNode, endPos);
-        range.deleteContents();
-        var el = this.getDocument().createElement('div');
-        el.innerHTML = html;
-        var frag = this.getDocument().createDocumentFragment(),
-            node,
-            lastNode;
-
-        while (node = el.firstChild) {
-          lastNode = frag.appendChild(node);
-        }
-
-        range.insertNode(frag); // Preserve the selection
-
-        if (lastNode) {
-          range = range.cloneRange();
-          range.setStartAfter(lastNode);
-          range.collapse(true);
-          sel.removeAllRanges();
-          sel.addRange(range);
-        }
+        sel.anchorNode.nodeValue = sel.anchorNode.nodeValue.substring(0, startPos) + html + sel.anchorNode.nodeValue.substring(endPos, sel.anchorNode.nodeValue.length);
+        range.setStart(sel.anchorNode, startPos + html.length);
+        range.collapse(true);
+        sel.removeAllRanges();
+        sel.addRange(range);
       }
     }, {
       key: "getWindowSelection",
