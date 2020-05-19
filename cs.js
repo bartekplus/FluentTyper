c0862c2b4c3b2143996b1c0a53c141407222c168
Bartosz Tomczyk <bartekplus@gmail.com>
2020-05-19 16:34:13 +0200

    Initial cleanup with eslint.

--- cs.js
+++ cs.js
@@ -1,290 +1,266 @@
-"use strict";
+'use strict';
+/* global Tribute */
 
-(function() {
+(function () {
+  var tributeArr = []
+  var observer = null
+  var pendingReq = null
+  var PRESAGE_PREDICTION_TIMEOUT_MS = 1000
 
-    var tributeArr = [];
-    var observer = null;
-    var pendingReq = null;
-    var PRESAGE_PREDICTION_TIMEOUT_MS = 1000;
+  function isHelperAttached (helperArr, elem) {
+    for (var i = 0; i < helperArr.length; i++) {
+      if (elem === helperArr[i].elem) {
+        return true
+      }
+    }
+    return false
+  }
 
-    function isHelperAttached(helperArr, elem) {
-        for (var i = 0; i < helperArr.length; i++) {
-            if (elem == helperArr[i].elem) {
-                return true;
-            }
+  function cancelPresageRequestTimeout (tributeId) {
+    if (tributeArr[tributeId].timeout) {
+      clearTimeout(tributeArr[tributeId].timeout)
+      tributeArr[tributeId].timeout = null
+    }
+  }
+
+  function setPresageRequestTimeout (tributeId) {
+    tributeArr[tributeId].timeout = setTimeout(function () {
+      pendingReq = null
+      tributeArr[tributeId].timeout = null
+      tributeArr[tributeId].done([])
+    }, PRESAGE_PREDICTION_TIMEOUT_MS)
+  }
+
+  function checkLastError () {
+    try {
+      if (chrome.runtime.lastError) {
+        console.log(chrome.runtime.lastError.message)
+      }
+    } catch (e) {};
+  }
+
+  function MutationCallback (mutationsList, observer) {
+    var nodesAdded = false
+    for (const mutation of mutationsList) {
+      if (mutation.type === 'childList') {
+        if (mutation.addedNodes) {
+          nodesAdded = true
         }
-        return false;
+      }
     }
+    if (nodesAdded) {
+      attachHelper()
+    }
+  };
 
-    function cancelPresageRequestTimeout(tributeId) {
-        if (tributeArr[tributeId].timeout) {
-            clearTimeout(tributeArr[tributeId].timeout);
-            tributeArr[tributeId].timeout = null;
+  function attachHelper () {
+    var selectors = ['textarea', 'input', '[contentEditable="true"]']
+
+    for (var selectorId = 0; selectorId < selectors.length; selectorId++) {
+      var elems = document.querySelectorAll(selectors[selectorId])
+      for (var i = 0; i < elems.length; i++) {
+        var elem = elems[i]
+
+        if (elem.getAttribute('type') === 'password') {
+          continue
         }
-    }
 
-    function setPresageRequestTimeout(tributeId) {
-        tributeArr[tributeId].timeout = setTimeout((function() {
-            pendingReq = null;
-            tributeArr[tributeId].timeout = null;
-            tributeArr[tributeId].done([]);
-        }), PRESAGE_PREDICTION_TIMEOUT_MS);
+        if (isHelperAttached(tributeArr, elem)) {
+          continue
+        }
+        var helperArrId = tributeArr.length
 
-    }
+        tributeArr.push({
+          tribute: null,
+          elem: elem,
+          done: null,
+          timeout: null,
+          requestId: 0
+        })
 
-    function checkLastError() {
-        try {
-            if (chrome.runtime.lastError) {
-                console.log(chrome.runtime.lastError.message);
-            }
-        } catch (e) {};
-    }
+        elem.addEventListener('tribute-replaced', function (e) {
+        })
 
-    function MutationCallback(mutationsList, observer) {
-        var nodesAdded = false;
-        for (let mutation of mutationsList) {
-            if (mutation.type === 'childList') {
-                for (let node of mutation.addedNodes) {
-                    //console.log(node);
-                    nodesAdded = true;
+        var tribute = new Tribute({
+          // symbol or string that starts the lookup
+          trigger: '',
+
+          // element to target for @mentions
+          iframe: null,
+
+          // class added in the flyout menu for active item
+          selectClass: 'highlight',
+
+          // class added to the menu container
+          containerClass: 'tribute-container',
+
+          // class added to each list item
+          itemClass: '',
+
+          // function called on select that returns the content to insert
+          selectTemplate: function (item) {
+            return item.original.value
+          },
+
+          // template for displaying item in menu
+          menuItemTemplate: function (item) {
+            return item.string
+          },
+
+          // template for when no match is found (optional),
+          // If no template is provided, menu is hidden.
+          noMatchTemplate: '',
+
+          // specify an alternative parent container for the menu
+          // container must be a positioned element for the menu to appear correctly ie. `position: relative;`
+          // default container is the body
+          menuContainer: document.body,
+
+          // column to search against in the object (accepts function or string)
+          lookup: 'key',
+
+          // column that contains the content to insert by default
+          fillAttr: 'value',
+
+          // REQUIRED: array of objects to match
+          values: (function (data, done) {
+            var localId = helperArrId
+            return function (data, done) {
+              if (!data) {
+                return done([])
+              }
+              tributeArr[localId].done = done
+              tributeArr[localId].requestId += 1
+              var message = {
+                command: 'predictReq',
+                context: {
+                  text: data,
+                  tributeId: localId,
+                  requestId: tributeArr[localId].requestId
                 }
-                for (let node of mutation.removedNodes) {
-                    //console.log(node);
-                }
+              }
+              // Check if we are waiting for a response
+              if (!pendingReq) {
+                // Set pending request
+                pendingReq = message
+                // Cancel old timeout Fn
+                cancelPresageRequestTimeout(localId)
 
-            } else if (mutation.type === 'attributes') {
-                //console.log('The ' + mutation.attributeName + ' attribute was modified.');
+                chrome.runtime.sendMessage(message, function (response) {
+                  checkLastError()
+                  setPresageRequestTimeout(localId)
+                })
+              } else {
+                pendingReq = message
+              }
             }
-        }
-        if (nodesAdded) {
-            attachHelper();
-        }
-    };
+          })(),
 
+          // specify whether a space is required before the trigger string
+          requireLeadingSpace: false,
 
-    function attachHelper() {
+          // specify whether a space is allowed in the middle of mentions
+          allowSpaces: false,
 
-        var selectors = ['textarea', 'input', '[contentEditable="true"]'];
+          // optionally specify a custom suffix for the replace text
+          // (defaults to empty space if undefined)
+          replaceTextSuffix: '',
 
-        for (var selectorId = 0; selectorId < selectors.length; selectorId++) {
+          // specify whether the menu should be positioned.  Set to false and use in conjuction with menuContainer to create an inline menu
+          // (defaults to true)
+          positionMenu: true,
 
-            var elems = document.querySelectorAll(selectors[selectorId]);
-            for (var i = 0; i < elems.length; i++) {
-                var elem = elems[i];
+          // when the spacebar is hit, select the current match
+          spaceSelectsMatch: false,
 
-                if (elem.getAttribute("type") === "password") {
-                    continue;
-                }
+          // turn tribute into an autocomplete
+          autocompleteMode: true,
 
-                if (isHelperAttached(tributeArr, elem)) {
-                    continue;
-                }
-                var helperArrId = tributeArr.length;
+          // Customize the elements used to wrap matched strings within the results list
+          // defaults to <span></span> if undefined
+          searchOpts: {
+            pre: '<span>',
+            post: '</span>',
+            skip: false // true will skip local search, useful if doing server-side search
+          },
 
-                tributeArr.push({
-                    tribute: null,
-                    elem: elem,
-                    done: null,
-                    timeout: null,
-                    requestId: 0
-                });
+          // specify the minimum number of characters that must be typed before menu appears
+          menuShowMinLength: 0
+        })
+        tributeArr[helperArrId].tribute = tribute
+        tribute.attach(elem)
+      }
+    }
+  }
 
-                elem.addEventListener("tribute-replaced", function(e) {
-                    var x = elem;
-                    //setTimeout((function() {
-                    // Triger predition after replacing text, without user interacction
-                    //e.target.dispatchEvent(new KeyboardEvent('keydown', { 'key': ' ' }));
-                    //e.target.dispatchEvent(new KeyboardEvent('keyup', { 'key': ' ' }));
-                    // }), 10);
-                });
+  function initializeFluentTyper () {
+    window.addEventListener('DOMContentLoaded', function (evt) {
+      initializeFluentTyper()
+    }, false)
 
-                var tribute = new Tribute({
-                    // symbol or string that starts the lookup
-                    trigger: "",
+    attachHelper()
+    if (!observer) {
+      var observerOptions = {
+        childList: true,
+        attributes: false,
+        subtree: true
+      }
+      observer = new MutationObserver(MutationCallback)
+      observer.observe(document.body, observerOptions)
+    }
+  }
 
-                    // element to target for @mentions
-                    iframe: null,
+  chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    checkLastError()
 
-                    // class added in the flyout menu for active item
-                    selectClass: 'highlight',
-
-                    // class added to the menu container
-                    containerClass: 'tribute-container',
-
-                    // class added to each list item
-                    itemClass: '',
-
-                    // function called on select that returns the content to insert
-                    selectTemplate: function(item) {
-                        return item.original.value;
-                    },
-
-                    // template for displaying item in menu
-                    menuItemTemplate: function(item) {
-                        return item.string;
-                    },
-
-                    // template for when no match is found (optional),
-                    // If no template is provided, menu is hidden.
-                    noMatchTemplate: "",
-
-                    // specify an alternative parent container for the menu
-                    // container must be a positioned element for the menu to appear correctly ie. `position: relative;`
-                    // default container is the body
-                    menuContainer: document.body,
-
-                    // column to search against in the object (accepts function or string)
-                    lookup: 'key',
-
-                    // column that contains the content to insert by default
-                    fillAttr: 'value',
-
-                    // REQUIRED: array of objects to match
-                    values: (function(data, done) {
-                        var localId = helperArrId;
-                        return function(data, done) {
-
-                            if (!data) {
-                                return done([]);
-                            }
-                            tributeArr[localId].done = done;
-                            tributeArr[localId].requestId += 1;
-                            var message = {
-                                command: 'predictReq',
-                                context: {
-                                    text: data,
-                                    tributeId: localId,
-                                    requestId: tributeArr[localId].requestId
-                                }
-                            };
-                            // Check if we are waiting for a response
-                            if (!pendingReq) {
-                                // Set pending request
-                                pendingReq = message;
-                                //Cancel old timeout Fn
-                                cancelPresageRequestTimeout(localId)
-
-                                chrome.runtime.sendMessage(message, function(response) {
-                                    checkLastError();
-                                    setPresageRequestTimeout(localId);
-                                });
-                            } else {
-                                pendingReq = message;
-                            }
-                        }
-                    })(),
-
-                    // specify whether a space is required before the trigger string
-                    requireLeadingSpace: false,
-
-                    // specify whether a space is allowed in the middle of mentions
-                    allowSpaces: false,
-
-                    // optionally specify a custom suffix for the replace text
-                    // (defaults to empty space if undefined)
-                    replaceTextSuffix: '',
-
-                    // specify whether the menu should be positioned.  Set to false and use in conjuction with menuContainer to create an inline menu
-                    // (defaults to true)
-                    positionMenu: true,
-
-                    // when the spacebar is hit, select the current match
-                    spaceSelectsMatch: false,
-
-                    // turn tribute into an autocomplete
-                    autocompleteMode: true,
-
-                    // Customize the elements used to wrap matched strings within the results list
-                    // defaults to <span></span> if undefined
-                    searchOpts: {
-                        pre: '<span>',
-                        post: '</span>',
-                        skip: false // true will skip local search, useful if doing server-side search
-                    },
-
-                    // specify the minimum number of characters that must be typed before menu appears
-                    menuShowMinLength: 0
-                });
-                tributeArr[helperArrId].tribute = tribute;
-                tribute.attach(elem);
-
+    switch (message.command) {
+      case 'predictResp':
+        // We were waiting for prediction
+        if (pendingReq) {
+          // Check if msg are equal
+          if (message.context.requestId === tributeArr[message.context.tributeId].requestId) {
+            var keyValPairs = []
+            for (var i = 0; i < message.context.predictions.length; i++) {
+              keyValPairs.push({
+                key: message.context.predictions[i],
+                value: message.context.predictions[i]
+              })
             }
-
+            // Cancel old timeout Fn
+            cancelPresageRequestTimeout(message.context.tributeId)
+            // Send prediction to TributeJs
+            tributeArr[message.context.tributeId].done(keyValPairs)
+          } else {
+            // Msq are not equal, ignore result and send pending msg
+            chrome.runtime.sendMessage(pendingReq, function (response) {
+              checkLastError()
+              setPresageRequestTimeout(message.context.tributeId)
+            })
+          }
+          pendingReq = null
         }
+        break
+      default:
+        console.log('Unknown message')
+        console.log(message)
+        break
+    }
+  })
+
+  function getConfig () {
+    var message = {
+      command: 'getConfig',
+      context: {}
     }
 
+    chrome.runtime.sendMessage(message, function (response) {
+      checkLastError()
 
-    function initializeFluentTyper() {
-
-        window.addEventListener("DOMContentLoaded", function(evt) {
-            initializeFluentTyper();
-        }, false);
-
-        attachHelper();
-        if (!observer) {
-            var observerOptions = {
-                childList: true,
-                attributes: false,
-                subtree: true
-            }
-            observer = new MutationObserver(MutationCallback);
-            observer.observe(document.body, observerOptions);
-        }
-    }
-
-    chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-        checkLastError();
-
-        switch (message.command) {
-            case "predictResp":
-                // We were waiting for prediction
-                if (pendingReq) {
-                    // Check if msg are equal
-                    if (message.context.requestId == tributeArr[message.context.tributeId].requestId) {
-                        var keyValPairs = [];
-                        for (var i = 0; i < message.context.predictions.length; i++) {
-                            keyValPairs.push({
-                                key: message.context.predictions[i],
-                                value: message.context.predictions[i]
-                            })
-                        }
-                        //Cancel old timeout Fn
-                        cancelPresageRequestTimeout(message.context.tributeId);
-                        // Send prediction to TributeJs
-                        tributeArr[message.context.tributeId].done(keyValPairs);
-                    } else {
-                        // Msq are not equal, ignore result and send pending msg 
-                        chrome.runtime.sendMessage(pendingReq, function(response) {
-                            checkLastError();
-                            setPresageRequestTimeout(message.context.tributeId);
-                        });
-                    }
-                    pendingReq = null;
-                }
-                break;
-            default:
-                console.log("Unknown message");
-                console.log(message);
-                break;
-        }
+      if (response.context.enabled) {
+        initializeFluentTyper()
+      }
     })
+  }
 
-
-    function getConfig() {
-        var message = {
-            command: 'getConfig',
-            context: {}
-        };
-
-        chrome.runtime.sendMessage(message, function(response) {
-            checkLastError();
-
-            if (response.context.enabled) {
-                initializeFluentTyper();
-            }
-        });
-
-    }
-
-    getConfig();
-})();
+  getConfig()
+})()
--- third_party/libpresage/libpresageHandler.js
+++ third_party/libpresage/libpresageHandler.js
@@ -1,81 +1,75 @@
-"use strict";
+'use strict'
 
-var presage = null;
+var presage = null
 
 var lastPrediction = {
-    past_stream: null,
-    predictions: null
+  past_stream: null,
+  predictions: null
 }
 
 var presageCallback = {
-    past_stream: "",
+  past_stream: '',
 
-    get_past_stream: function() {
-        return this.past_stream;
-    },
+  get_past_stream: function () {
+    return this.past_stream
+  },
 
-    get_future_stream: function() {
-        return "";
-    }
-};
+  get_future_stream: function () {
+    return ''
+  }
+}
 var Module = {
-    onRuntimeInitialized: function() {
-        var pcObject = Module.PresageCallback.implement(presageCallback);
-        presage = new Module.Presage(pcObject, "resources_js/presage.xml");
-    }
-};
-
-function convertString(s) {
-    var str = ""
-    if (typeof(s) === 'string' || s instanceof String) {
-        if (s.endsWith(' ')) {
-            str = s.trim() + ' ';
-        } else {
-            str = s.trim();
-        }
-    }
-    return str;
+  onRuntimeInitialized: function () {
+    var pcObject = Module.PresageCallback.implement(presageCallback)
+    presage = new Module.Presage(pcObject, 'resources_js/presage.xml')
+  }
 }
 
-window.addEventListener('message', function(event) {
-    var command = event.data.command;
-    switch (command) {
-        case 'predictReq':
-            var context = event.data.context;
-            var past_stream = convertString(event.data.context.text);
-            if (!past_stream || !presage) {
-                // Nothing to do here
-            } else if (past_stream == lastPrediction.past_stream) {
-                context.predictions = lastPrediction.predictions;
-                var message = {
-                    command: 'predictResp',
-                    context: context
-                };
-                event.source.postMessage(message, event.origin);
+function convertString (s) {
+  var str = ''
+  if (typeof (s) === 'string' || s instanceof String) {
+    if (s.endsWith(' ')) {
+      str = s.trim() + ' '
+    } else {
+      str = s.trim()
+    }
+  }
+  return str
+}
 
-            } else {
-                var predictions = [];
-                presageCallback.past_stream = past_stream
-                var predictionsNative = presage.predict();
-                if (predictionsNative.size()) {
+window.addEventListener('message', function (event) {
+  var command = event.data.command
+  switch (command) {
+    case 'predictReq':
+      var context = event.data.context
+      var pastStream = convertString(event.data.context.text)
+      var message = {
+        command: 'predictResp',
+        context: context
+      }
+      if (!pastStream || !presage) {
+        // Nothing to do here
+      } else if (pastStream === lastPrediction.pastStream) {
+        message.context.predictions = lastPrediction.predictions
 
-                    for (var i = 0; i < predictionsNative.size(); i++) {
-                        predictions.push(predictionsNative.get(i));
-                    }
-                    context.predictions = predictions;
-                    var message = {
-                        command: 'predictResp',
-                        context: context
-                    };
-                    event.source.postMessage(message, event.origin);
-                    lastPrediction.past_stream = past_stream;
-                    lastPrediction.predictions = predictions;
-                }
-            }
-            break;
+        event.source.postMessage(message, event.origin)
+      } else {
+        var predictions = []
+        presageCallback.pastStream = pastStream
+        var predictionsNative = presage.predict()
+        if (predictionsNative.size()) {
+          for (var i = 0; i < predictionsNative.size(); i++) {
+            predictions.push(predictionsNative.get(i))
+          }
+          message.context.predictions = predictions
+          event.source.postMessage(message, event.origin)
+          lastPrediction.pastStream = pastStream
+          lastPrediction.predictions = predictions
+        }
+      }
+      break
 
             // case 'somethingElse':
             //   ...
-    }
-
-});
+  }
+})
