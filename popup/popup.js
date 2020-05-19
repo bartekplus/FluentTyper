75126b8411a4429862903bcf1828c4c05b61a11b
Bartosz Tomczyk <bartekplus@gmail.com>
2020-05-19 23:10:29 +0200

    Cleanup popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,62 +1,54 @@
-/*jslint browser:true */
-/*global Store,chrome*/
+/* jslint browser:true */
+/* global Store,chrome */
 
-var settings = new Store("settings");
+import { getDomain, isDomainOnList, removeDomainFromList, addDomainToList } from '../utils.js'
 
+var settings = new Store('settings')
 
-function init() {
-    "use strict";
-    chrome.tabs.query({ active: true }, function(tabs) {
-        var currentTab = tabs[0];
-        var whiteList = [];
-        var bcgPage = chrome.extension.getBackgroundPage();
-        var urlNode = document.getElementById("checkboxDomainLabel");
-        var checkboxNode = document.getElementById("checkboxDomainInput");
-        var checkboxEnableNode = document.getElementById("checkboxEnableInput");
+function init () {
+  'use strict'
+  chrome.tabs.query({ active: true }, function (tabs) {
+    var currentTab = tabs[0]
+    var urlNode = document.getElementById('checkboxDomainLabel')
+    var checkboxNode = document.getElementById('checkboxDomainInput')
+    var checkboxEnableNode = document.getElementById('checkboxEnableInput')
 
-        var domainURL = getDomain(currentTab.url);
-        var opMode = settings.get("operatingMode");
-        urlNode.innerText = "Enable autocomplete on: " + domainURL;
+    var domainURL = getDomain(currentTab.url)
+    var opMode = settings.get('operatingMode')
+    urlNode.innerText = 'Enable autocomplete on: ' + domainURL
 
-        if (isDomainOnList(settings, domainURL)) {
-            checkboxNode.checked = opMode === "blacklist" ? false : true;
-        } else {
-            checkboxNode.checked = opMode === "blacklist" ? true : false;
-        }
+    if (isDomainOnList(settings, domainURL)) {
+      checkboxNode.checked = opMode !== 'blacklist'
+    } else {
+      checkboxNode.checked = opMode === 'blacklist'
+    }
 
-        checkboxEnableNode.checked = settings.get("enable");
-        document.getElementById("runOptions").href = chrome.extension.getURL("options.html");
-
-    });
+    checkboxEnableNode.checked = settings.get('enable')
+    document.getElementById('runOptions').href = chrome.extension.getURL('options.html')
+  })
 }
 
-function addRemoveDomain() {
-    chrome.tabs.query({ active: true }, function(tabs) {
-        var currentTab = tabs[0];
-        var whiteList = [];
-        var bcgPage = chrome.extension.getBackgroundPage();
-        var urlNode = document.getElementById("dupa");
-        var domainURL = getDomain(currentTab.url);
-        var idx;
-        var opMode = settings.get("operatingMode");
+function addRemoveDomain () {
+  chrome.tabs.query({ active: true }, function (tabs) {
+    var currentTab = tabs[0]
+    var domainURL = getDomain(currentTab.url)
 
-        if (isDomainOnList(settings, domainURL)) {
-            removeDomainFromList(settings, domainURL);
-        } else {
-            addDomainToList(settings, domainURL);
-        }
-    });
+    if (isDomainOnList(settings, domainURL)) {
+      removeDomainFromList(settings, domainURL)
+    } else {
+      addDomainToList(settings, domainURL)
+    }
+  })
 }
 
-function toggleOnOff() {
-    settings.set("enable", !settings.get("enable"));
+function toggleOnOff () {
+  settings.set('enable', !settings.get('enable'))
 }
 
+init()
 
-init();
-
-window.document.addEventListener("DOMContentLoaded", function() {
-    "use strict";
-    window.document.getElementById("checkboxDomainInput").addEventListener("click", addRemoveDomain);
-    window.document.getElementById("checkboxEnableInput").addEventListener("click", toggleOnOff);
-});
+window.document.addEventListener('DOMContentLoaded', function () {
+  'use strict'
+  window.document.getElementById('checkboxDomainInput').addEventListener('click', addRemoveDomain)
+  window.document.getElementById('checkboxEnableInput').addEventListener('click', toggleOnOff)
+})
