96dfa7f234c181893e8921f28afed80a27bf924e
Bartosz Tomczyk <bartekplus@gmail.com>
2020-05-19 16:34:13 +0200

    Initial cleanup with eslint.

--- background.js
+++ background.js
@@ -1,100 +1,99 @@
-"use strict";
+'use strict'
+/* global Store */
+import { isDomainOnList, checkLastError } from './utils.js'
 
-var settings = new Store("settings");
+var settings = new Store('settings')
 
 // Check whether new version is installed
-chrome.runtime.onInstalled.addListener(function(details) {
-    if (details.reason == "install") {
-        //chrome.tabs.create({
-        //    url: "options/index.html"
-        //});
-    } else if (details.reason == "update") {
-        var thisVersion = chrome.runtime.getManifest().version;
-        console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
-        //chrome.tabs.create({url: "options/index.html"});
-    }
-});
+chrome.runtime.onInstalled.addListener(function (details) {
+  if (details.reason === 'install') {
+    // chrome.tabs.create({
+    //    url: "options/index.html"
+    // });
+  } else if (details.reason === 'update') {
+    var thisVersion = chrome.runtime.getManifest().version
+    console.log('Updated from ' + details.previousVersion + ' to ' + thisVersion + '!')
+    // chrome.tabs.create({url: "options/index.html"});
+  }
+})
 
-//setDefault Values
-function setDefault(settings) {
-    "use strict";
-    //First Run - no value stored
-    if (settings.get("enable") === undefined) {
-        settings.set("enable", true);
-    }
-    if (settings.get("operatingMode") === undefined) {
-        settings.set("operatingMode", "blacklist");
-    }
+// setDefault Values
+function setDefault (settings) {
+  'use strict'
+  // First Run - no value stored
+  if (settings.get('enable') === undefined) {
+    settings.set('enable', true)
+  }
+  if (settings.get('operatingMode') === undefined) {
+    settings.set('operatingMode', 'blacklist')
+  }
 }
 
-function sendMsgToSandbox(message) {
-    var iframe = document.getElementById('sandboxFrame');
-    iframe.contentWindow.postMessage(message, '*');
+function sendMsgToSandbox (message) {
+  var iframe = document.getElementById('sandboxFrame')
+  iframe.contentWindow.postMessage(message, '*')
 }
 
+function isEnabledForDomain (domainURL) {
+  var enabledForDomain = settings.get('enable')
+  if (enabledForDomain) {
+    var opMode = settings.get('operatingMode')
+    enabledForDomain = (opMode === 'blacklist')
 
-function isEnabledForDomain(domainURL) {
-    var enabledForDomain = settings.get("enable");
-    if (enabledForDomain) {
-        var opMode = settings.get("operatingMode");
-        enabledForDomain = (opMode === "blacklist") ? true : false;
-
-        if (isDomainOnList(settings, domainURL)) {
-            if (opMode === "blacklist") {
-                enabledForDomain = false;
-            } else {
-                enabledForDomain = true;
-            }
-        }
+    if (isDomainOnList(settings, domainURL)) {
+      if (opMode === 'blacklist') {
+        enabledForDomain = false
+      } else {
+        enabledForDomain = true
+      }
     }
-    return enabledForDomain;
+  }
+  return enabledForDomain
 }
-
 
 // Called when a message is passed.  We assume that the content script
 // wants to show the page action.
-function onRequest(request, sender, sendResponse) {
-    "use strict";
-    var respMsg = {};
+function onRequest (request, sender, sendResponse) {
+  'use strict'
+  var respMsg = {}
 
-    checkLastError();
+  checkLastError()
 
-    request.context.tabId = sender.tab.id;
-    request.context.frameId = sender.frameId;
+  request.context.tabId = sender.tab.id
+  request.context.frameId = sender.frameId
 
+  switch (request.command) {
+    case 'predictReq':
+      sendMsgToSandbox(request)
+      break
 
-    switch (request.command) {
-        case 'predictReq':
-            sendMsgToSandbox(request);
-            break;
+    case 'getConfig':
+      respMsg = {
+        command: 'getConfig',
+        context: {
+          enabled: isEnabledForDomain(sender.tab.url)
+        }
+      }
+  }
 
-        case 'getConfig':
-            respMsg = {
-                command: "getConfig",
-                context: {
-                    enabled: isEnabledForDomain(sender.tab.url),
-                }
-            };
-    }
-
-    // Return nothing to let the connection be cleaned up.
-    sendResponse(respMsg);
+  // Return nothing to let the connection be cleaned up.
+  sendResponse(respMsg)
 }
 
 // Listen for the content script to send a message to the background page.
-chrome.runtime.onMessage.addListener(onRequest);
+chrome.runtime.onMessage.addListener(onRequest)
 
-setDefault(settings);
+setDefault(settings)
 
-function receiveMessage(event) {
-    checkLastError();
-    chrome.tabs.sendMessage(event.data.context.tabId, event.data, {
-        frameId: event.data.context.frameId
-    });
+function receiveMessage (event) {
+  checkLastError()
+  chrome.tabs.sendMessage(event.data.context.tabId, event.data, {
+    frameId: event.data.context.frameId
+  })
 }
 
-window.addEventListener("message", receiveMessage, false);
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    checkLastError();
-    chrome.pageAction.show(tabId);
-});
+window.addEventListener('message', receiveMessage, false)
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  checkLastError()
+  chrome.pageAction.show(tabId)
+})
--- utils.js
+++ utils.js
@@ -1,71 +1,70 @@
-"use strict";
+'use strict'
 
-function getDomain(url) {
+function getDomain (url) {
+  url = url.replace(/(https?:\/\/)?(www.)?/i, '')
 
-    url = url.replace(/(https?:\/\/)?(www.)?/i, '');
+  if (url.indexOf('/') !== -1) {
+    return url.split('/')[0]
+  }
 
-    if (url.indexOf('/') !== -1) {
-        return url.split('/')[0];
-    }
-
-    return url;
-
+  return url
 }
 
-function isDomainOnList(settings, domainURL) {
-    var ret = false;
-    var domainList = [];
-    domainURL = getDomain(domainURL);
+function isDomainOnList (settings, domainURL) {
+  var ret = false
+  var domainList = []
+  domainURL = getDomain(domainURL)
 
-    var domainListAsStr = settings.get("domainList");
-    if (domainListAsStr) {
-        domainList = domainListAsStr.split("|@|");
-    }
+  var domainListAsStr = settings.get('domainList')
+  if (domainListAsStr) {
+    domainList = domainListAsStr.split('|@|')
+  }
 
-    for (var i = 0; i < domainList.length; i++) {
-        if (domainURL.match(domainList[i])) {
-            ret = true;
-            break;
-        }
+  for (var i = 0; i < domainList.length; i++) {
+    if (domainURL.match(domainList[i])) {
+      ret = true
+      break
     }
-    return ret;
+  }
+  return ret
 }
 
+function addDomainToList (settings, domainURL) {
+  var domainListAsStr = settings.get('domainList')
+  if (domainListAsStr) {
+    domainListAsStr = domainListAsStr.split('|@|')
+  } else {
+    domainListAsStr = []
+  }
+  domainListAsStr.push(domainURL)
 
-function addDomainToList(settings, domainURL) {
-    var domainListAsStr = settings.get("domainList");
-    if (domainListAsStr) {
-        domainListAsStr = domainListAsStr.split("|@|");
-    } else {
-        domainListAsStr = [];
-    }
-    domainListAsStr.push(domainURL);
-
-    settings.set("domainList", domainListAsStr.join("|@|"));
+  settings.set('domainList', domainListAsStr.join('|@|'))
 }
 
-function removeDomainFromList(settings, domainURL) {
-    var domainList = [];
-    var domainListAsStr = settings.get("domainList");
+function removeDomainFromList (settings, domainURL) {
+  var domainList = []
+  var domainListAsStr = settings.get('domainList')
 
-    domainURL = getDomain(domainURL);
-    if (domainListAsStr) {
-        domainList = domainListAsStr.split("|@|");
-    }
+  domainURL = getDomain(domainURL)
+  if (domainListAsStr) {
+    domainList = domainListAsStr.split('|@|')
+  }
 
-    for (var i = 0; i < domainList.length; i++) {
-        if (domainURL.match(domainList[i])) {
-            domainList.splice(i, 1);
-            settings.set("domainList", domainList.join("|@|"));
-            break;
-        }
+  for (var i = 0; i < domainList.length; i++) {
+    if (domainURL.match(domainList[i])) {
+      domainList.splice(i, 1)
+      settings.set('domainList', domainList.join('|@|'))
+      break
     }
+  }
 }
 
-function checkLastError() {
-    try {
-        if (chrome.runtime.lastError) {
-            console.log(chrome.runtime.lastError.message);
-        }
-    } catch (e) {};
+function checkLastError () {
+  try {
+    if (chrome.runtime.lastError) {
+      console.log(chrome.runtime.lastError.message)
+    }
+  } catch (e) {};
 }
+
+export { checkLastError, removeDomainFromList, addDomainToList, isDomainOnList, getDomain }
