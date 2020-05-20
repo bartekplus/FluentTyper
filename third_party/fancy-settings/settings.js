066df8c112ba0d3088953d2c4cb2cbfbe0752d4c
Bartosz Tomczyk <bartekplus@gmail.com>
2020-05-20 17:22:09 +0200

    Run eslint on fancy-setting, convert it to modules.

--- third_party/fancy-settings/settings.js
+++ third_party/fancy-settings/settings.js
@@ -1,14 +1,16 @@
-window.addEvent("domready", function () {
-    // Option 1: Use the manifest:
-    new FancySettings.initWithManifest(function (settings) {
-        settings.manifest.addDomainBtn.addEvent("action", function () {
-        	var domainURL = settings.manifest.domain.get();
-        	var elem = new Element("option", {"value": domainURL, "text": domainURL });
-        	settings.manifest.domainList.add(domainURL);
-        });
-        
-        settings.manifest.removeDomainBtn.addEvent("action", function () {
-        	settings.manifest.domainList.remove();
-        });
-    });
-});
+import { FancySettingsWithManifest } from './js/classes/fancy-settings.js'
+
+window.addEvent('domready', function () {
+  // Option 1: Use the manifest:
+  (() => new FancySettingsWithManifest(function (settings) {
+    settings.manifest.addDomainBtn.addEvent('action', function () {
+      var domainURL = settings.manifest.domain.get()
+      settings.manifest.domainList.add(domainURL)
+    })
+
+    settings.manifest.removeDomainBtn.addEvent('action', function () {
+      settings.manifest.domainList.remove()
+    })
+  })
+  )()
+})
