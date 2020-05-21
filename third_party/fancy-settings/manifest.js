42e927210887a6a0b1799a16be00b06adb107942
Bartosz Tomczyk <bartekplus@gmail.com>
2020-05-21 14:01:31 +0200

    Add donate button.

--- third_party/fancy-settings/manifest.js
+++ third_party/fancy-settings/manifest.js
@@ -1,5 +1,43 @@
 import { i18n } from "./i18n.js";
 
+var donateHTML =
+  "<p>" +
+  "Creating and maintaining FluentTyper has required - and still " +
+  "does - a considerable amount of work and effort. If you like " +
+  "FluentTyper, then you may wish to support FluentTyper " +
+  "development by making a voluntary donation. This will also " +
+  "enable future enhancement to the extension. " +
+  "</p>" +
+  "<form " +
+  'action="https://www.paypal.com/cgi-bin/webscr"' +
+  'method="post"' +
+  'target="_top"' +
+  ">" +
+  '<input type="hidden" name="cmd" value="_donations" />' +
+  '<input type="hidden" name="business" value="FNA6UN4JRSRTU" />' +
+  "<input" +
+  '  type="hidden"' +
+  '  name="item_name"' +
+  '  value="Support for Development of FluentTyper"' +
+  "/>" +
+  '<input type="hidden" name="currency_code" value="USD" />' +
+  "<input" +
+  '  type="image"' +
+  '  src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"' +
+  '  border="0"' +
+  '  name="submit"' +
+  '  title="PayPal - The safer, easier way to pay online!"' +
+  '  alt="Donate with PayPal button"' +
+  "/>" +
+  "<img" +
+  '  alt=""' +
+  '  border="0"' +
+  '  src="https://www.paypal.com/en_US/i/scr/pixel.gif"' +
+  '  width="1"' +
+  '  height="1"' +
+  "/>" +
+  "</form>";
+
 // SAMPLE
 var manifest = {
   name: "FluentTyper Settings",
@@ -80,6 +118,13 @@ var manifest = {
       name: "Version",
       type: "description",
       text: chrome.runtime.getManifest().version,
+    },
+    {
+      tab: i18n.get("About"),
+      group: i18n.get("Donate"),
+      name: "Donate",
+      type: "description",
+      text: donateHTML,
     },
   ],
 };
