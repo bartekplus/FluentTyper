11e6860bf5b43c9d77ab6b52ffeb18d4ea86bba4
Bartosz Tomczyk <bartekplus@gmail.com>
2020-05-27 23:11:20 +0200

    Add addon ID for Firefox.

--- fixManifestForFF.sh
+++ fixManifestForFF.sh
@@ -2,3 +2,4 @@
 
 jq ".content_security_policy = \"script-src 'self' 'unsafe-eval'; object-src 'self'\"" manifest.json | sponge manifest.json
 jq ".permissions = [ \"tabs\", \"<all_urls>\"]" manifest.json | sponge manifest.json
+jq ".browser_specific_settings += { \"gecko\": { \"id\": \"bartekplus+fluenttyper@example.com\" } }" manifest.json | sponge manifest.json
