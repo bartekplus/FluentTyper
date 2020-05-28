483efbfa7efddfaa4cf4458782e61adb0db7dd6b
Bartosz Tomczyk <bartekplus@gmail.com>
2020-05-28 19:02:52 +0200

    Remove not need file.

--- fixManifestForFF.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-jq ".content_security_policy = \"script-src 'self' 'unsafe-eval'; object-src 'self'\"" manifest.json | sponge manifest.json
-jq ".permissions = [ \"tabs\", \"<all_urls>\"]" manifest.json | sponge manifest.json
-jq ".browser_specific_settings += { \"gecko\": { \"id\": \"bartekplus+fluenttyper@example.com\" } }" manifest.json | sponge manifest.json
