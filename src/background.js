cf770222ef35fce270de2af8a1469b095f67ac79
Bartosz Tomczyk <bartekplus@gmail.com>
2021-08-12 20:47:45 +0200

    backgroun.js code cleanup.
    Make it more readable and easier to maintain.

--- src/background.js
+++ src/background.js
@@ -1,176 +1,188 @@
 "use strict";
 
 import { isDomainOnList, checkLastError } from "./utils.js";
-
 import { Store } from "./third_party/fancy-settings/lib/store.js";
 
-const settings = new Store("settings");
+(function () {
+  const SANDBOX_FRAME_INIT_TIME_MS = 3000;
+  const SANDBOX_FRAME_LOAD_TIME_MS = 500;
+  class BackgrounPage {
+    constructor() {
+      this.settings = new Store("settings");
 
-// Check whether new version is installed
-chrome.runtime.onInstalled.addListener(function (details) {
-  checkLastError();
-  if (details.reason === "install") {
-    chrome.tabs.create({
-      url: "new_installation/index.html",
-    });
-  } else if (details.reason === "update") {
-    const thisVersion = chrome.runtime.getManifest().version;
-    console.log(
-      "Updated from " + details.previousVersion + " to " + thisVersion + "!"
-    );
-    // chrome.tabs.create({url: "options/index.html"});
-  }
-});
-
-function sendMsgToSandbox(message) {
-  const iframe = document.getElementById("sandboxFrame");
-  iframe.contentWindow.postMessage(message, "*");
-}
-
-function isEnabledForDomain(domainURL) {
-  let enabledForDomain = settings.get("enable");
-  if (enabledForDomain) {
-    enabledForDomain = false;
-
-    if (isDomainOnList(settings, domainURL)) {
-      enabledForDomain = true;
-    } else if (domainURL.indexOf(chrome.runtime.getURL("")) !== -1) {
-      enabledForDomain = true;
+      chrome.runtime.onInstalled.addListener(this.onInstalled.bind(this));
+      chrome.runtime.onMessage.addListener(this.onMessage.bind(this));
+      chrome.commands.onCommand.addListener(this.onCommand.bind(this));
+      window.addEventListener(
+        "message",
+        this.onMessageFromSandbox.bind(this),
+        false
+      );
+      window.addEventListener(
+        "DOMContentLoaded",
+        this.onDOMContentLoaded.bind(this)
+      );
     }
-  }
-  return enabledForDomain;
-}
 
-// Called when a message is passed.  We assume that the content script
-// wants to show the page action.
-function onRequest(request, sender, sendResponse) {
-  checkLastError();
+    onInstalled(details) {
+      checkLastError();
+      if (details.reason === "install") {
+        chrome.tabs.create({
+          url: "new_installation/index.html",
+        });
+      } else if (details.reason === "update") {
+        const thisVersion = chrome.runtime.getManifest().version;
+        console.log(
+          "Updated from " + details.previousVersion + " to " + thisVersion + "!"
+        );
+        // chrome.tabs.create({url: "options/index.html"});
+      }
+    }
 
-  request.context.tabId = sender.tab.id;
-  request.context.frameId = sender.frameId;
+    sendMsgToSandbox(message) {
+      const iframe = document.getElementById("sandboxFrame");
+      iframe.contentWindow.postMessage(message, "*");
+    }
 
-  switch (request.command) {
-    case "contentScriptPredictReq":
-      request.context.lang = settings.get("language");
-      request.command = "backgroundPagePredictReq";
-      sendMsgToSandbox(request);
-      break;
-    case "status":
-      // showPageAction(sender.tab.id, request.context.enabled);
-      break;
+    isEnabledForDomain(domainURL) {
+      let enabledForDomain = this.settings.get("enable");
+      if (enabledForDomain) {
+        enabledForDomain = false;
 
-    case "optionsPageConfigChange":
-      updatePresageConfig();
-      break;
+        if (isDomainOnList(this.settings, domainURL)) {
+          enabledForDomain = true;
+        } else if (domainURL.indexOf(chrome.runtime.getURL("")) !== -1) {
+          enabledForDomain = true;
+        }
+      }
+      return enabledForDomain;
+    }
 
-    case "contentScriptGetConfig":
-      const respMsg = {
+    //Messages from option page and content script
+    onMessage(request, sender, sendResponse) {
+      checkLastError();
+
+      request.context.tabId = sender.tab.id;
+      request.context.frameId = sender.frameId;
+
+      switch (request.command) {
+        case "contentScriptPredictReq":
+          request.context.lang = this.settings.get("language");
+          request.command = "backgroundPagePredictReq";
+          this.sendMsgToSandbox(request);
+          break;
+        case "status":
+          // showPageAction(sender.tab.id, request.context.enabled);
+          break;
+
+        case "optionsPageConfigChange":
+          this.updatePresageConfig();
+          break;
+
+        case "contentScriptGetConfig":
+          const respMsg = {
+            command: "backgroundPageSetConfig",
+            context: {
+              enabled: this.isEnabledForDomain(sender.tab.url),
+              useEnter: this.settings.get("useEnter"),
+            },
+          };
+          sendResponse(respMsg);
+          break;
+      }
+    }
+
+    //Messages from option page and content script
+    onMessageFromSandbox(event) {
+      checkLastError();
+
+      switch (event.data.command) {
+        case "sandBoxPredictResp":
+          // Make sure that tabId is still valid
+          chrome.tabs.get(event.data.context.tabId, function (tab) {
+            checkLastError();
+
+            if (tab) {
+              // Update command to indicate orign of the message
+              event.data.command = "backgroundPagePredictResp";
+              chrome.tabs.sendMessage(event.data.context.tabId, event.data, {
+                frameId: event.data.context.frameId,
+              });
+            }
+          });
+          break;
+
+        default:
+          console.log("Unkown message:");
+          console.log(event);
+          break;
+      }
+    }
+
+    toggleOnOffActiveTab() {
+      chrome.tabs.query(
+        { active: true, lastFocusedWindow: true },
+        function (tabs) {
+          checkLastError();
+          if (tabs.length === 1) {
+            const currentTab = tabs[0];
+
+            const message = {
+              command: "backgroundPageToggle",
+              context: {},
+            };
+
+            chrome.tabs.sendMessage(currentTab.id, message);
+          }
+        }
+      );
+    }
+
+    onCommand(command) {
+      switch (command) {
+        case "toggle-ft-active-tab":
+          this.toggleOnOffActiveTab();
+          break;
+
+        default:
+          console.log("Unknown command: ", command);
+          break;
+      }
+    }
+
+    updatePresageConfig() {
+      this.sendMsgToSandbox({
         command: "backgroundPageSetConfig",
         context: {
-          enabled: isEnabledForDomain(sender.tab.url),
-          useEnter: settings.get("useEnter"),
+          lang: this.settings.get("language"),
+          numSuggestions: this.settings.get("numSuggestions"),
+          minWordLenghtToPredict: this.settings.get("minWordLenghtToPredict"),
+          predictNextWordAfterWhiteSpace: this.settings.get(
+            "predictNextWordAfterWhiteSpace"
+          ),
         },
-      };
-      sendResponse(respMsg);
-      break;
-  }
-
-  // SendResponse
-}
-
-// Listen for the content script to send a message to the background page.
-chrome.runtime.onMessage.addListener(onRequest);
-
-function receiveMessage(event) {
-  checkLastError();
-
-  switch (event.data.command) {
-    case "sandBoxPredictResp":
-      // Make sure that tabId is still valid
-      chrome.tabs.get(event.data.context.tabId, function (tab) {
-        checkLastError();
-
-        if (tab) {
-          // Update command to indicate orign of the message
-          event.data.command = "backgroundPagePredictResp";
-          chrome.tabs.sendMessage(event.data.context.tabId, event.data, {
-            frameId: event.data.context.frameId,
-          });
-        }
       });
-      break;
-
-    default:
-      console.log("Unkown message:");
-      console.log(event);
-      break;
-  }
-}
-
-/*
-function setPageActionIcon(tabId, isActive) {
-  chrome.browserAction.setBadgeText({
-    text: isActive ? "On" : "Off",
-  });
-}
-
-function showPageAction(tabId, isActive) {
-  setPageActionIcon(tabId, isActive);
-}
-*/
-
-window.addEventListener("message", receiveMessage, false);
-
-function toggleOnOffActiveTab() {
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
-    checkLastError();
-    if (tabs.length === 1) {
-      const currentTab = tabs[0];
-
-      const message = {
-        command: "backgroundPageToggle",
-        context: {},
-      };
-
-      chrome.tabs.sendMessage(currentTab.id, message);
     }
-  });
-}
 
-chrome.commands.onCommand.addListener(function (command) {
-  switch (command) {
-    case "toggle-ft-active-tab":
-      toggleOnOffActiveTab();
-      break;
+    onSandboxFrameLoaded() {
+      setTimeout(
+        this.updatePresageConfig.bind(this),
+        SANDBOX_FRAME_INIT_TIME_MS
+      );
+    }
 
-    default:
-      console.log("Unknown command: ", command);
-      break;
+    // Trigger config update after sandboxFrame 'load' event
+    onDOMContentLoaded(event) {
+      if (navigator.userAgent.indexOf("Chrome") !== -1) {
+        setTimeout(
+          this.onSandboxFrameLoaded.bind(this),
+          SANDBOX_FRAME_LOAD_TIME_MS
+        );
+      } else {
+        const iframe = document.getElementById("sandboxFrame");
+        iframe.addEventListener("load", this.onSandboxFrameLoaded.bind(this));
+      }
+    }
   }
-});
-
-function updatePresageConfig() {
-  sendMsgToSandbox({
-    command: "backgroundPageSetConfig",
-    context: {
-      lang: settings.get("language"),
-      numSuggestions: settings.get("numSuggestions"),
-      minWordLenghtToPredict: settings.get("minWordLenghtToPredict"),
-      predictNextWordAfterWhiteSpace: settings.get(
-        "predictNextWordAfterWhiteSpace"
-      ),
-    },
-  });
-}
-
-// Trigger config update after sandboxFrame 'load' event
-window.addEventListener("DOMContentLoaded", (event) => {
-  const iframe = document.getElementById("sandboxFrame");
-  if (navigator.userAgent.indexOf("Chrome") !== -1) {
-    setTimeout(updatePresageConfig, 3000);
-  } else {
-    iframe.addEventListener("load", function () {
-      updatePresageConfig();
-    });
-  }
-});
+  const backgroundPage = new BackgrounPage();
+})();
