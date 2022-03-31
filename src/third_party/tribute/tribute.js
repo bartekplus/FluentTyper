b343eba1a738bbce461f334c68ff041de20a1125
Bartosz Tomczyk <bartekplus@gmail.com>
2022-03-31 17:44:04 +0200

    Update Tribue.js
    Add space while reverting autocomplete.

--- src/third_party/tribute/tribute.js
+++ src/third_party/tribute/tribute.js
@@ -199,10 +199,11 @@
             if (this.tribute.events.updateSelection(_el) && this.tribute.current.mentionPosition == this.tribute.lastReplacement.mentionPosition + this.tribute.lastReplacement.content.length) {
               e.preventDefault();
               e.stopImmediatePropagation();
+              const addSpace = this.tribute.lastReplacement.content !== this.tribute.lastReplacement.content.trimEnd();
               this.tribute.current = { ...this.tribute.lastReplacement
               };
               this.tribute.current.mentionText = this.tribute.lastReplacement.content;
-              this.tribute.replaceText(this.tribute.lastReplacement.mentionText, e, null);
+              this.tribute.replaceText(this.tribute.lastReplacement.mentionText + (addSpace ? " " : ""), e, null);
             }
 
             this.tribute.lastReplacement = null;
