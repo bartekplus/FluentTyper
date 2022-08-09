ef1dd04a8e2bcb3a8420efbccada9eaed4f5f0a7
Bartosz Tomczyk <bartekplus@gmail.com>
2022-08-09 18:35:39 +0200

    Fixes #10

--- src/third_party/tribute/tribute.js
+++ src/third_party/tribute/tribute.js
@@ -16,7 +16,7 @@
     }
 
     static digits() {
-      return ["Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9", "Digit0"];
+      return ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
     }
 
     static modifiers() {
@@ -65,7 +65,7 @@
 
         if (instance.tribute.selectByDigit) {
           TributeEvents.digits().forEach((key, index) => {
-            if (key === event.code && instance.tribute.isActive) {
+            if (key === event.key && instance.tribute.isActive) {
               const count = instance.tribute.current.filteredItems.length;
 
               if (index < count) {
