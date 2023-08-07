5250e570a4847e7aa9ef5bd68e6d5ef17c0daa9e
Bartosz Tomczyk <bartekplus@gmail.com>
2023-08-07 19:53:26 +0200

    Cleanup

--- scripts/rebuild_all.sh
+++ scripts/rebuild_all.sh
@@ -5,8 +5,8 @@ set -eu
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # rebuild
-${SCRIPT_DIR}/rebuild_ngram_db.sh
-${SCRIPT_DIR}/rebuild_libpresage.sh
+"${SCRIPT_DIR}"/rebuild_ngram_db.sh
+"${SCRIPT_DIR}"/rebuild_libpresage.sh
 
 # repack stuff
 
--- scripts/rebuild_ngram_db.sh
+++ scripts/rebuild_ngram_db.sh
@@ -5,7 +5,7 @@ set -eu
 
 MAX_FILES=20
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source ${SCRIPT_DIR}/langs.sh
+source "${SCRIPT_DIR}"/langs.sh
 OSCAR_CORUPS_VERSION="OSCAR-2301"
 LANGUAGE_DETECTION_PROB="0.9"
 HARMFUL_SCORE="250.0"
@@ -21,7 +21,7 @@ download_and_extract() {
     LANG=$2
     WORK_DIR=$3
 
-    if [ ! -f ${FILE_PATH} ]; then
+    if [ ! -f "${FILE_PATH}" ]; then
         git lfs pull --include "${FILE_PATH}.zst"
         if [ ! -f "${FILE_PATH}.zst" ]; then
             echo "${FILE_PATH}.zst not found"
@@ -39,19 +39,19 @@ download_and_extract() {
         select ( .metadata.identification.prob >= '${LANGUAGE_DETECTION_PROB}') |
         select ( .metadata.harmful_pp >= '${HARMFUL_SCORE}') |
         .content ' \
-    ${FILE_PATH} >> "${WORK_DIR}/${LANG}_sentences_${i}.txt" && \
-    hunspell -i utf-8 -d ${LANGS_HUNSPELL[${lang}]} -G -L "${WORK_DIR}/${LANG}_sentences_${i}.txt" >  "${WORK_DIR}/${LANG}_sentences_checked_${i}.txt"  && \
+    "${FILE_PATH}" >> "${WORK_DIR}/${LANG}_sentences_${i}.txt" && \
+    hunspell -i utf-8 -d "${LANGS_HUNSPELL[${lang}]}" -G -L "${WORK_DIR}/${LANG}_sentences_${i}.txt" >  "${WORK_DIR}/${LANG}_sentences_checked_${i}.txt"  && \
     rm -rf "${WORK_DIR}/${LANG}_sentences_${i}.txt" &
 }
 
-cd ${SCRIPT_DIR}
+cd "${SCRIPT_DIR}"
 if [ ! -d ${OSCAR_CORUPS_VERSION} ]; then
     GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/datasets/oscar-corpus/${OSCAR_CORUPS_VERSION}
 fi
 
 cd ${OSCAR_CORUPS_VERSION}
 WORK_DIR="${SCRIPT_DIR}/tmp"
-mkdir -p ${WORK_DIR}
+mkdir -p "${WORK_DIR}"
 git lfs install
 
 for lang in "${LANGS[@]}"
@@ -60,18 +60,18 @@ do
         echo "Low quality HR dataset, skipping"
         continue
     fi
-    FILE_COUNT=$(ls ${lang}_meta/${lang}_meta_part_*.zst |wc -l)
+    FILE_COUNT=$(ls "${lang}"_meta/"${lang}"_meta_part_*.zst |wc -l)
     FILE_STEP=$((${FILE_COUNT} / ${MAX_FILES}))
     FILE_MAX=$((${FILE_STEP} * ${MAX_FILES}))
 
     SENTENCES_FILE="${WORK_DIR}/${lang}_sentences.txt"
-    rm -rf ${SENTENCES_FILE}
+    rm -rf "${SENTENCES_FILE}"
     
     for i in $(seq 1 $FILE_STEP $FILE_MAX)
     do
         FILE_NAME="${lang}_meta_part_${i}.jsonl"
         FILE_PATH="${lang}_meta/${FILE_NAME}"
-        download_and_extract $FILE_PATH $lang $WORK_DIR
+        download_and_extract "$FILE_PATH" "$lang" "$WORK_DIR"
     done    
 done
 
@@ -85,13 +85,13 @@ do
         continue
     fi
     # Merge spellchecked files
-    cat ${WORK_DIR}/${lang}_sentences_checked_*.txt > "${WORK_DIR}/${lang}_sentences_checked.txt"
-    rm -rf ${WORK_DIR}/${lang}_sentences_checked_*.txt 
+    cat "${WORK_DIR}"/"${lang}"_sentences_checked_*.txt > "${WORK_DIR}/${lang}_sentences_checked.txt"
+    rm -rf "${WORK_DIR}"/"${lang}"_sentences_checked_*.txt 
     
     # Generate ngrams 
-    python3 ${SCRIPT_DIR}/gen_ngram.py -i "${WORK_DIR}/${lang}_sentences_checked.txt" -l en
+    python3 "${SCRIPT_DIR}"/gen_ngram.py -i "${WORK_DIR}/${lang}_sentences_checked.txt" -l en
     # generate marisa-trie database from ngrams
-    python3 ${SCRIPT_DIR}/ngramtxt2marisa.py --overwrite --output ${SCRIPT_DIR}/../resources_js/${lang}/ngrams_db/ --inputfile "${WORK_DIR}/${lang}_sentences_checked_ngram_merged.txt"
+    python3 "${SCRIPT_DIR}"/ngramtxt2marisa.py --overwrite --output "${SCRIPT_DIR}"/../resources_js/"${lang}"/ngrams_db/ --inputfile "${WORK_DIR}/${lang}_sentences_checked_ngram_merged.txt"
 done
 
 
