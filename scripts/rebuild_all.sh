ad2bed573466377fc0d304f109234dc3e2c152a9
Bartosz Tomczyk <bartekplus@gmail.com>
2025-05-05 07:11:14 +0200

    Run it for all supported languages

--- scripts/rebuild_all.sh
+++ scripts/rebuild_all.sh
@@ -4,8 +4,25 @@ set -eu
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+declare -A SUPPORTED_LANGUAGES=(
+  [en]="en_US"
+  [fr]="fr_FR"
+  [hr]="hr_HR"
+  [es]="es_ES"
+  [el]="el_GR"
+  [sv]="sv_SE"
+  [de]="de_DE"
+  [pl]="pl_PL"
+  [pt]="pt_BR"
+)
+
+for lang_short in "${!SUPPORTED_LANGUAGES[@]}"; do
+  lang_variant="${SUPPORTED_LANGUAGES[$lang_short]}"
+  echo "Starting rebuild for language: ${lang_short}, variant: ${lang_variant}"
+  "${SCRIPT_DIR}"/rebuild_ngram_db.sh -l "${lang_short}" -v "${lang_variant}"
+done
+
 # rebuild
-"${SCRIPT_DIR}"/rebuild_ngram_db.sh
 "${SCRIPT_DIR}"/rebuild_libpresage.sh
 
 # repack stuff
