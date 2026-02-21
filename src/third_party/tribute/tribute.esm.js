2ed6204c062a5b512e1e50c5eb245ea4fb2c908a
Bartosz Tomczyk <bartekplus@gmail.com>
2026-02-21 09:36:22 +0100

    Fix inline suggestion disappear bug

--- src/third_party/tribute/tribute.esm.js
+++ src/third_party/tribute/tribute.esm.js
@@ -76,7 +76,13 @@ class TributeEvents {
       const item = instance.tribute.current.inlineSuggestionItem;
       if (item) {
         const content = instance.tribute.current.collection.selectTemplate(item);
-        if (content !== null) instance.tribute.replaceText(content, event, item);
+        if (content !== null) {
+          setTimeout(() => {
+            instance.tribute.replaceText(content, event, item);
+            instance.tribute.hideMenu();
+          }, 0);
+          return; /* return early to avoid hiding menu too early */
+        }
       }
       instance.tribute.hideMenu();
       return;
