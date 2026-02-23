e473a0547805d68edf74d47d68d181cbd09ab254
Bartosz Tomczyk <bartekplus@gmail.com>
2026-02-23 10:27:09 +0100

    WIP: script refactor

--- scripts/rebuild_all.sh
+++ scripts/rebuild_all.sh
@@ -1,30 +1,6 @@
 #!/usr/bin/env bash
 
-set -eu
+set -euo pipefail
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-declare -A SUPPORTED_LANGUAGES=(
-  [en]="en_US"
-  [fr]="fr_FR"
-  [hr]="hr_HR"
-  [es]="es_ES"
-  [el]="el_GR"
-  [sv]="sv_SE"
-  [de]="de_DE"
-  [pl]="pl_PL"
-  [pt]="pt_BR"
-)
-
-for lang_short in "${!SUPPORTED_LANGUAGES[@]}"; do
-  lang_variant="${SUPPORTED_LANGUAGES[$lang_short]}"
-  echo "Starting rebuild for language: ${lang_short}, variant: ${lang_variant}"
-  "${SCRIPT_DIR}"/rebuild_ngram_db.sh -l "${lang_short}" -v "${lang_variant}"
-done
-
-# rebuild
-"${SCRIPT_DIR}"/rebuild_libpresage.sh
-
-# repack stuff
-
-BUILD_CMD=""
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
+exec "${SCRIPT_DIR}/rebuild_all.py" "$@"
