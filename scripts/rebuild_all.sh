573fd01300dadd3734dd5d96e7089be9e9efb261
Bartosz Tomczyk <bartekplus@gmail.com>
2026-02-23 10:27:10 +0100

    Remove old bash scripts

--- scripts/rebuild_all.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
-exec "${SCRIPT_DIR}/rebuild_all.py" "$@"
--- scripts/rebuild_ngram_db.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
-exec python3 "${SCRIPT_DIR}/rebuild_ngram_db.py" "$@"
