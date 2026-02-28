d4c251b22e2b1b559eaba202adb4e10301afd33d
Bartosz Tomczyk <bartekplus@gmail.com>
2026-02-28 19:55:09 +0100

    feat: localize text expander documentation with i18n support

--- src/third_party/fancier-settings/manifest.js
+++ src/third_party/fancier-settings/manifest.js
@@ -74,17 +74,17 @@ const supportLinksHTML =
   </div>';
 const variablesDocumentationHTML =
   '<div class="text-expander-help" style="font-size: 0.9em; opacity: 0.9;"> \
-    <p style="margin-bottom: 0.5rem;"><strong>Supported Variables:</strong></p> \
+    <p style="margin-bottom: 0.5rem;"><strong>' + i18n.get("text_expander_vars_supported") + '</strong></p> \
     <ul style="list-style-type: disc; margin-left: 1.5rem; margin-bottom: 0;"> \
-      <li><code>\\${time}</code> - Current time (e.g., 10:30 AM)</li> \
-      <li><code>\\${date}</code> - Current date (e.g., 2026-02-28)</li> \
-      <li><code>\\${date:+1d}</code> - Date math (use + or -, then number, then d/w/m/y)</li> \
-      <li><code>\\${datetime}</code> - Current date and time</li> \
-      <li><code>\\${uuid}</code> - Generates a random unique identifier (UUID)</li> \
-      <li><code>\\${random:A|B|C}</code> - Picks a random option from the provided list</li> \
-      <li><code>\\${page_url}</code> - URL of the current page</li> \
-      <li><code>\\${page_title}</code> - Title of the current page</li> \
-      <li><code>\\${page_domain}</code> - Domain of the current page</li> \
+      <li><code>\\${time}</code> - ' + i18n.get("text_expander_var_time") + '</li> \
+      <li><code>\\${date}</code> - ' + i18n.get("text_expander_var_date") + '</li> \
+      <li><code>\\${date:+1d}</code> - ' + i18n.get("text_expander_var_date_math") + '</li> \
+      <li><code>\\${datetime}</code> - ' + i18n.get("text_expander_var_datetime") + '</li> \
+      <li><code>\\${uuid}</code> - ' + i18n.get("text_expander_var_uuid") + '</li> \
+      <li><code>\\${random:A|B|C}</code> - ' + i18n.get("text_expander_var_random") + '</li> \
+      <li><code>\\${page_url}</code> - ' + i18n.get("text_expander_var_page_url") + '</li> \
+      <li><code>\\${page_title}</code> - ' + i18n.get("text_expander_var_page_title") + '</li> \
+      <li><code>\\${page_domain}</code> - ' + i18n.get("text_expander_var_page_domain") + '</li> \
     </ul> \
   </div>';
 const IS_DEV_BUILD =
