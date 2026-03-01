d27d31ec6d731ccea46b03d92579fd4b7b037c86
Bartosz Tomczyk <bartekplus@gmail.com>
2026-03-01 09:16:16 +0100

    Lint

--- tests/e2e/puppeteer-extension.test.ts
+++ tests/e2e/puppeteer-extension.test.ts
@@ -735,7 +735,7 @@ describeE2E(`Extension E2E Test [${BROWSER_TYPE}]`, () => {
       await newInstallationPage.waitForSelector("#permissions-success", { visible: true });
 
       // Validate that the request was called with right arguments
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+       
       const reqArgs = await newInstallationPage.evaluate(
         () => (window as any).__lastPermissionRequest,
       );
